a49dbc1a6de70a4647d45de3881df5360c88a12c
lureiny <lureniy@gmail.com>
2019-04-02 01:04:23 +0800

    feat(small_tools.py): 添加日志格式化输出方法

--- common_units/small_tools.py
+++ common_units/small_tools.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+import time
 
 
 def get_time():
     return datetime.now()
 
+
+def print_info(info):
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ": " + info)
